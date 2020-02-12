1ade4c6a85d1fc5f234662107edd15737e5fc06d
AyushVerma2 <ayushverma2@gmail.com>
2020-02-13 00:31:12 +0800

    server data

--- src/main/java/com/spring/config/configserver/ConfigServerApplication.java
+++ src/main/java/com/spring/config/configserver/ConfigServerApplication.java
@@ -3,8 +3,9 @@ package com.spring.config.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableAutoConfiguration
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
